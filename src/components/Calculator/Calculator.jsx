8f9741790c9c73fb51ae3a9af8bdba28b2b60bde
sonpm283 <sonpm79382@gmail.com>
2023-08-27 23:17:49 +0700

    Temperature Calculate

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -2,6 +2,24 @@ import React, { Component } from 'react'
 import TemperatureInput from './TemperatureInput'
 import BoillingVerdict from './BoillingVerdict'
 
+
+const tryConvert = (temperator, scaleInput) => {
+  const input = parseFloat(temperator)
+
+  // if(typeof input !== 'number') return ''
+  if(Number.isNaN(input)) return ''
+
+  let output
+
+  if(scaleInput === 'f') {
+    output = (input - 32) / 1.8
+  }else {
+    output = input * 1.8 + 32
+  }
+
+  return Math.round(output * 1000) / 1000
+}
+
 export class Calculator extends Component {
 
   constructor(props) {
@@ -21,22 +39,27 @@ export class Calculator extends Component {
   }
   
   render() {
+    const {scale, temperator} = this.state
+    
+    const celsius = scale === 'f' ? tryConvert(temperator, scale) : temperator
+    const fahrenheit = scale === 'c' ? tryConvert(temperator, scale) : temperator
+
     return (
       <div>
         <h2>Calculator</h2>
         <TemperatureInput 
-          title='Celsius'
-          temperature = {this.state.temperator}
+          title='C'
+          temperature = {celsius}
           onTemperatureChange={this.handleChange('c')}
         />
 
         <TemperatureInput 
-          title='Fahrenheit'
-          temperature = {this.state.temperator}
+          title='F'
+          temperature = {fahrenheit}
           onTemperatureChange={this.handleChange('f')}
         />
 
-        <BoillingVerdict celsius={100} />
+        <BoillingVerdict celsius={Number(celsius)} />
       </div>
     )
   }
